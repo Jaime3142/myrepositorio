6629b291f288d0c00bcca0117980818143e0a4c6
Jaime <jnc0021@alu.medac.es>
2025-11-26 11:11:40 +0100

    .

--- practicaDocker/php/html/listar.php
+++ practicaDocker/php/html/listar.php
@@ -1,5 +1,6 @@
 <?php
-include 'conexion.php';
+require_once 'conexion.php';
+header('Content-Type: application/json');
 
 $res = mysqli_query($conexion, "SELECT * FROM alumnos ORDER BY id DESC");
 $datos = [];
